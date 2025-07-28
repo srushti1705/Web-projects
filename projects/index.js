8c2c1ef9fb5e7d460fcee84f344e7e29b657fe35
Srushti <srushtiwadi17@gmail.com>
2025-07-28 19:59:06 +0530

    Added age verification label and button.

--- projects/index.js
+++ projects/index.js
@@ -16,7 +16,6 @@ const PI = 3.1419;
 let radius;
 let circumference;
 
-
 document.getElementById("mySubmit").onclick = function() {
     radius = document.getElementById("myText").value;
     radius = Number(radius);
@@ -24,4 +23,29 @@ document.getElementById("mySubmit").onclick = function() {
     document.getElementById("myH3").textContent = circumference;
 }
 
+const myText2 = document.getElementById("myText2");
+const mySubmit2 = document.getElementById("mySubmit2");
+const resultElement = document.getElementById("resultElement");
+let age;
+mySubmit2.onclick = function(){
+
+    age = myText2.value;
+    age = Number(age);
+    
+    if(age >= 100){
+        resultElement.textContent = `You are TOO OLD to enter this site`;
+    }
+    else if(age == 0){
+        resultElement.textContent = `You can't enter. You were just born.`
+    }
+    else if(age >= 18){
+        resultElement.textContent = `You are old enough to enter this site`
+    }
+    else if(age < 0){
+        resultElement.textContent = `Your age can't be below 0`;
+    }
+    else{
+        resultElement.textContent = `You must be 18+ to enter this site`;
+    }
+}
 //window.alert(`This is an alert!`);
