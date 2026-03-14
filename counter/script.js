7e1c3955b57cbb3cdd1ae1b29c693aa745de48af
Srushti <srushtiwadi17@gmail.com>
2026-03-14 19:56:43 +0530

    modified the counter program

--- counter/script.js
+++ counter/script.js
@@ -7,14 +7,26 @@ let count = 0;
 increasebtn.onclick = function() {
     count++;
     countLabel.textContent = count;
+    updateCountColor();
 }
 
 decreasebtn.onclick = function() {
     count--;
     countLabel.textContent = count;
+    updateCountColor();
 }
 
 resetbtn.onclick = function() {
     count = 0;
     countLabel.textContent = count;
+    countLabel.style.color = "black";
+} 
+
+function updateCountColor() {
+ if (count > 0) {
+        countLabel.style.color = "green";
+    }
+else if (count < 0) {
+        countLabel.style.color = "red";
+    }
 }
