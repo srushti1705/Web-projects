3000aec694d237ad333298a943591a9243dc80ea
Srushti <srushtiwadi17@gmail.com>
2026-03-15 13:00:10 +0530

    modified number guessing game

--- numguessinggame/index.js
+++ numguessinggame/index.js
@@ -1,33 +1,25 @@
-const minNum = 1;
-const maxNum = 100;
-const answer = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+let gameResult = document.getElementById("gameResult");
+let userInput = document.getElementById("userInput");
+let randomNumber = Math.ceil(Math.random() * 100);
+let guesses = 0;
+function checkGuess() {
+  let guessedNumber = parseInt(userInput.value);
+  guesses++;
 
-let attempts = 0;
-let guess;
-let running = true;
-
-while(running){
-    
-    guess = window.prompt(`Guess a number between ${minNum} - ${maxNum}`);
-    guess = Number(guess);
-    
-    if(isNaN(guess)){
-        window.alert("Please enter a valid number");
-    }
-    else if(guess < minNum || guess > maxNum){
-        window.alert("Please enter a valid number");
-    }
-    else{
-        attempts++;
-        if(guess < answer){
-            window.alert("TOO LOW! TRY AGAIN!");
-        }
-        else if(guess > answer){
-            window.alert("TOO HIGH! TRY AGAIN!");
-        }
-        else{
-            window.alert(`CORRECT! The answer was ${answer}. It took you ${attempts} attempts`);
-            running = false;
-        }
-    }
+  if (guessedNumber > randomNumber) {
+    gameResult.textContent = "Too High! Try Again.";
+    gameResult.style.backgroundColor = "#1e217c";
+  }
+  else if (guessedNumber < randomNumber) {
+    gameResult.textContent = "Too Low! Try Again.";
+    gameResult.style.backgroundColor = "#1e217c";
+  }
+  else if (guessedNumber === randomNumber) {
+    gameResult.textContent = `Congratulations! You got it right in ${guesses} guesses.`;
+    gameResult.style.backgroundColor = "green";
+  }
+  else {
+    gameResult.textContent = "Please provide a valid input.";
+    gameResult.style.backgroundColor = "#1e217c";
+  }
 }
