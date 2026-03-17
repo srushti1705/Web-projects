c719e45188e04e1e2ea5567a0be8324cd4fc163e
Srushti <srushtiwadi17@gmail.com>
2026-03-17 18:18:30 +0530

    color picker program

---
+++ colorPicker/index.js
@@ -0,0 +1,19 @@
+function changeButton1() {
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#e0e0e0";
+    document.getElementById("selectedColorHexCode").textContent = "#e0e0e0";
+}
+
+function changeButton2() {
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#6fcf97";
+    document.getElementById("selectedColorHexCode").textContent = "#6fcf97";
+}
+
+function changeButton3() {
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#56ccf2";
+    document.getElementById("selectedColorHexCode").textContent = "#56ccf2";
+}
+
+function changeButton4() {
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#bd6bd9";
+    document.getElementById("selectedColorHexCode").textContent = "#bd6bd9";
+}
