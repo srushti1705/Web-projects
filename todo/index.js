332173e56b063db7796d751f1ecd43c89ee45275
Srushti <srushtiwadi17@gmail.com>
2026-04-04 00:27:10 +0530

    refactor: generate todo items dynamically from array

--- todo/index.js
+++ todo/index.js
@@ -1,30 +1,47 @@
 let todoItemsContainer = document.getElementById("todoItemsContainer");
-let todoElement = document.createElement("li");
-todoElement.classList.add("todo-item-container", "d-flex", "flex-row");
-todoItemsContainer.appendChild(todoElement);
 
-let inputElement = document.createElement("input");
-inputElement.type = "checkbox";
-inputElement.id = "checkboxInput";
-inputElement.classList.add("checkbox-input");
-todoElement.appendChild(inputElement);
+let todoList = [
+    {
+        text: "Learn HTML"
+    },
+    {
+        text: "Learn CSS"
+    },
+    {
+        text: "Learn JavaScript"
+    }
+];
 
-let labelContainer = document.createElement("div");
-labelContainer.classList.add("label-container", "d-flex", "flex-row");
-todoElement.appendChild(labelContainer);
+function createAndAppendTodo(todo) {
+    let todoElement = document.createElement("li");
+    todoElement.classList.add("todo-item-container", "d-flex", "flex-row");
+    todoItemsContainer.appendChild(todoElement);
+    
+    let inputElement = document.createElement("input");
+    inputElement.type = "checkbox";
+    inputElement.id = "checkboxInput";
+    inputElement.classList.add("checkbox-input");
+    todoElement.appendChild(inputElement);
 
-let labelElement = document.createElement("label");
-labelElement.setAttribute("for", "checkboxInput");
-labelElement.classList.add("checkbox-label");
-labelElement.textContent = "Learn HTML";
-labelContainer.appendChild(labelElement);
+    let labelContainer = document.createElement("div");
+    labelContainer.classList.add("label-container", "d-flex", "flex-row");
+    todoElement.appendChild(labelContainer);
 
-let deleteIconContainer = document.createElement("div");
-deleteIconContainer.classList.add("delete-icon-container");
-labelContainer.appendChild(deleteIconContainer);
+    let labelElement = document.createElement("label");
+    labelElement.setAttribute("for", "checkboxInput");
+    labelElement.classList.add("checkbox-label");
+    labelElement.textContent = todo.text;
+    labelContainer.appendChild(labelElement);
 
-let deleteIcon = document.createElement("i");
-deleteIcon.classList.add("far", "fa-trash-alt", "delete-icon");
-deleteIconContainer.appendChild(deleteIcon);
+    let deleteIconContainer = document.createElement("div");
+    deleteIconContainer.classList.add("delete-icon-container");
+    labelContainer.appendChild(deleteIconContainer);
 
-console.log(todoItemsContainer);
+    let deleteIcon = document.createElement("i");
+    deleteIcon.classList.add("far", "fa-trash-alt", "delete-icon");
+    deleteIconContainer.appendChild(deleteIcon);
+}
+
+for (let todo of todoList) {
+    createAndAppendTodo(todo);
+}
